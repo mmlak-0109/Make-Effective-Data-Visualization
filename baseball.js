daddc04e9f3ca9560dd51e02ea2a68d6938f7761
Matthew Miller <mmlak_0109@yahoo.com>
2017-04-23 22:35:59 +0900

    Upload file
    Added code to make the legend interactive

--- baseball.js
+++ baseball.js
@@ -33,32 +33,43 @@ function draw_the_numbers(data) {
         //Define x-axis variable and title
         var x = chart.addCategoryAxis("x", "handedness");
         x.title = "Handedness";
+        x.fontSize = "14px";
 
         //Define y-axis scales and title that update when chart is updated by clicking the button
         if (chartType === "Total Handedness") {
             var y = chart.addMeasureAxis("y", "name");
             y.title = "Total";
+            y.fontSize = "14px";
             var series = chart.addSeries(null, dimple.plot.bar);
+            series.tooltipFontSize = "14px";
         } else if (chartType === "Average Homeruns") {
             var y = chart.addMeasureAxis("y", "HR");
             y.title = "Average Homeruns";
+            y.fontSize = "14px";
             var series = chart.addSeries(null, dimple.plot.bar);
             series.aggregate = dimple.aggregateMethod.avg;
+            series.tooltipFontSize = "14px";
         } else if (chartType === "Average Batting Average") {
             var y = chart.addMeasureAxis("y", "avg");
             y.title = "Average Batting Average";
+            y.fontSize = "14px";
             var series = chart.addSeries(null, dimple.plot.bar);
             series.aggregate = dimple.aggregateMethod.avg;
+            series.tooltipFontSize = "14px";
         } else if (chartType === "Average Height") {
             var y = chart.addMeasureAxis("y", "height");
-            y.title = "Average Height";
+            y.title = "Average Height (inches)";
+            y.fontSize = "14px";
             var series = chart.addSeries(null, dimple.plot.bar);
             series.aggregate = dimple.aggregateMethod.avg;
+            series.tooltipFontSize = "14px";
         } else if (chartType === "Average Weight") {
             var y = chart.addMeasureAxis("y", "weight");
-            y.title = "Average Weight";
+            y.title = "Average Weight (pounds)";
+            y.fontSize = "14px";
             var series = chart.addSeries(null, dimple.plot.bar);
             series.aggregate = dimple.aggregateMethod.avg;
+            series.tooltipFontSize = "14px";
         }
 
         chart.draw(1000);
@@ -99,7 +110,6 @@ function draw_the_numbers(data) {
         svg.selectAll("*").remove();
         chartUpdate(d);
     });
-
 }
 
 function draw_relationships(data) {
@@ -111,61 +121,155 @@ function draw_relationships(data) {
         .attr("height", "635px");
 
     //Define chart type for the initial visualization
-    var chartType = "Homeruns - Batting Average";
+    var chartType = "Batting Average - Homeruns";
 
     function chartUpdate(chartType) {
 
         var chart = new dimple.chart(svg, data);
-        chart.addLegend(150, 40, "top");
+        var legend = chart.addLegend(150, 40, "top");
+        legend.fontSize = "14px";
 
         //Define x-axis and y-axis variables, scales and titles that update when chart is updated by clicking the button
-        if (chartType === "Homeruns - Batting Average") {
+        if (chartType === "Batting Average - Homeruns") {
             //Define  title and bar-chart order
             var x = chart.addMeasureAxis("x", "HR");
             x.title = "Homeruns";
+            x.fontSize = "14px";
             var y = chart.addMeasureAxis("y", "avg");
             y.title = "Batting Average";
+            y.fontSize = "14px";
             var series = chart.addSeries(["name", "handedness"], dimple.plot.bubble);
-        } else if (chartType === "Height - Batting Average") {
+            series.tooltipFontSize = "14px";
+        } else if (chartType === "Batting Average - Height") {
             var x = chart.addCategoryAxis("x", "height");
-            x.title = "Height";
+            x.title = "Height (inches)";
+            x.fontSize = "14px";
             var y = chart.addMeasureAxis("y", "avg");
             y.title = "Batting Average";
+            y.fontSize = "14px";
             var series = chart.addSeries(["name", "handedness"], dimple.plot.bubble);
-        } else if (chartType === "Weight - Batting Average") {
+            series.tooltipFontSize = "14px";
+        } else if (chartType === "Batting Average - Weight") {
             var x = chart.addCategoryAxis("x", "weight");
-            x.title = "Weight";
+            x.title = "Weight (pounds)";
+            x.fontSize = "12px";
             var y = chart.addMeasureAxis("y", "avg");
             y.title = "Batting Average";
+            y.fontSize = "12px";
             var series = chart.addSeries(["name", "handedness"], dimple.plot.bubble);
-        } else if (chartType === "Height - Homeruns") {
+            series.tooltipFontSize = "14px";
+        } else if (chartType === "Homeruns - Height") {
             var x = chart.addCategoryAxis("x", "height");
-            x.title = "Height";
+            x.title = "Height (inches)";
+            x.fontSize = "14px";
             var y = chart.addMeasureAxis("y", "HR");
             y.title = "Homeruns";
+            y.fontSize = "14px";
             var series = chart.addSeries(["name", "handedness"], dimple.plot.bubble);
-        } else if (chartType === "Weight - Homeruns") {
+            series.tooltipFontSize = "14px";
+        } else if (chartType === "Homeruns - Weight") {
             var x = chart.addCategoryAxis("x", "weight");
-            x.title = "Weight";
+            x.title = "Weight (pounds)";
+            x.fontSize = "12px";
             var y = chart.addMeasureAxis("y", "HR");
             y.title = "Homeruns";
+            y.fontSize = "12px";
             var series = chart.addSeries(["name", "handedness"], dimple.plot.bubble);
+            series.tooltipFontSize = "14px";
         } else if (chartType === "Height - Weight") {
-            var x = chart.addCategoryAxis("x", "height");
-            x.title = "Height";
-            var y = chart.addCategoryAxis("y", "weight");
-            y.title = "Weight";
+            var x = chart.addCategoryAxis("x", "weight");
+            x.title = "Weight (pounds)";
+            x.fontSize = "12px";
+            var y = chart.addCategoryAxis("y", "height");
+            y.title = "Height (inches)";
+            y.fontSize = "12px";
             var series = chart.addSeries(["name", "handedness"], dimple.plot.bubble);
+            series.tooltipFontSize = "14px";
         }
 
         chart.draw(1000);
+        x.shapes.selectAll("text").attr("transform", function (d) {
+            if (chartType === "Batting Average - Weight") {
+                return d3.select(this).attr("transform") + "translate(0, 10) rotate(-45)";
+            } else if (chartType === "Homeruns - Weight") {
+                return d3.select(this).attr("transform") + "translate(0, 10) rotate(-45)";
+            } else if (chartType === "Height - Weight") {
+                return d3.select(this).attr("transform") + "translate(0, 10) rotate(-45)";
+            }
+        });
+
+        // This is a critical step.  By doing this we orphan the legend. This
+        // means it will not respond to graph updates.  Without this the legend
+        // will redraw when the chart refreshes removing the unchecked item and
+        // also dropping the events we define below.
+        chart.legends = [];
+
+        // This block simply adds the legend title. I put it into a d3 data
+        // object to split it onto 2 lines.  This technique works with any
+        // number of lines, it isn't dimple specific.
+        svg.selectAll("title_text")
+            .data(["Click legend to show/hide handedness:"])
+            .enter()
+            .append("text")
+            .attr("x", 100)
+            .attr("y", function (d, i) {
+                return 30 + i * 14;
+            })
+            .style("font-family", "sans-serif")
+            .style("font-size", "14px")
+            .style("color", "Black")
+            .text(function (d) {
+                return d;
+            });
+
+        // Get a unique list of Owner values to use when filtering
+        var filterValues = dimple.getUniqueValues(data, "handedness");
+        // Get all the rectangles from our now orphaned legend
+        legend.shapes.selectAll("rect")
+            // Add a click event to each rectangle
+            .on("click", function (e) {
+                // This indicates whether the item is already visible or not
+                var hide = false;
+                var newFilters = [];
+                // If the filters contain the clicked shape hide it
+                filterValues.forEach(function (f) {
+                    if (f === e.aggField.slice(-1)[0]) {
+                        hide = true;
+                    } else {
+                        newFilters.push(f);
+                    }
+                });
+                // Hide the shape or show it
+                if (hide) {
+                  d3.select(this).style("opacity", 0.2);
+                } else {
+                  newFilters.push(e.aggField.slice(-1)[0]);
+                  d3.select(this).style("opacity", 0.8);
+                }
+                // Update the filters
+                filterValues = newFilters;
+                // Filter the data
+                chart.data = dimple.filterData(data, "handedness", filterValues);
+                // Passing a duration parameter makes the chart animate. Without
+                // it there is no transition
+                chart.draw(1000);
+                x.shapes.selectAll("text").attr("transform", function (d) {
+                    if (chartType === "Batting Average - Weight") {
+                        return d3.select(this).attr("transform") + "translate(0, 10) rotate(-45)";
+                    } else if (chartType === "Homeruns - Weight") {
+                        return d3.select(this).attr("transform") + "translate(0, 10) rotate(-45)";
+                    } else if (chartType === "Height - Weight") {
+                        return d3.select(this).attr("transform") + "translate(0, 10) rotate(-45)";
+                    }
+                });
+            });
     }
 
-        //Call chart update function for the first plot using "Homeruns - Batting Average"
-        chartUpdate(chartType);
+    //Call chart update function for the first plot using "Homeruns - Batting Average"
+    chartUpdate(chartType);
 
     //Define options for buttons
-    var stat = ["Homeruns - Batting Average", "Height - Batting Average", "Weight - Batting Average", "Height - Homeruns", "Weight - Homeruns", "Height - Weight"];
+    var stat = ["Batting Average - Homeruns", "Batting Average - Height", "Batting Average - Weight", "Homeruns - Height", "Homeruns - Weight", "Height - Weight"];
 
     //Define buttons
     var buttons = d3.select("#relationships_buttons")
